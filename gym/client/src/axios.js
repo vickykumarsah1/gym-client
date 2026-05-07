c8df6d59465b21f7a02b34bec3eea5303127e876
vickykumarsah1 <vkdbg307047@gmail.com>
2026-05-08 01:31:38 +0530

    Create axios.js

---
+++ gym/client/src/axios.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const api = axios.create({
+  baseURL: "https://gym-management-two-eta.vercel.app"
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
